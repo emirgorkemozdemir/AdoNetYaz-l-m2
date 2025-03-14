b34358c899a3f568bbc275d30538b73386110a6e
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2025-03-14 21:43:18 +0300

    Add project files.

---
+++ AdoNetYazılım2/Controllers/AraclarController.cs
@@ -0,0 +1,24 @@
+﻿using AdoNetYazılım2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdoNetYazılım2.Controllers
+{
+    public class AraclarController : Controller
+    {
+        // veritabanı referansı her yerden bu arkadaşı kullanabilirsiniz.
+        OtoServisDBEntities db = new OtoServisDBEntities();
+
+        [HttpGet]
+        public ActionResult AraclarıYukle()
+        {
+            List<araclar> arac_listesi = db.araclars.ToList();
+            return View(arac_listesi);
+        }
+
+       
+    }
+}
---
+++ AdoNetYazılım2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdoNetYazılım2.MvcApplication" Language="C#" %>
